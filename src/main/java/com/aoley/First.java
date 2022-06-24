987fd8b0eac3ac4506ac6997fa9cd6ca9dd23662
Aoley <1307643895@qq.com>
2022-06-24 18:19:06 +0800

    第一次提交

---
+++ src/main/java/com/aoley/First.java
@@ -0,0 +1,11 @@
+package com.aoley;
+
+/**
+ * @author Aoley
+ * @date 2022/6/24 16:10
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
